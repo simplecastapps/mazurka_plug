92359209215edd22c7444dc2e5718f0a2cc317f9
David McHealy <david@simplecast.com>
2020-11-16 10:16:45 -0500

    Another update related to elixir 1.11

--- mix.exs
+++ mix.exs
@@ -22,6 +22,8 @@ defmodule Mazurka.Plug.Mixfile do
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev},
+      # Used in mazurka, referenced here via macro
+      {:mimetype_parser, "~> 0.1.0"}
       {:fugue, ">= 0.1.0"},
       {:html_builder, github: "simplecastapps/html_builder"},
       {:mazurka, github: "simplecastapps/mazurka"},
